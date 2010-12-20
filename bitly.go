4e42153aa34ce52e331d5e41722b8755ec1b1ebf
vermi <sloth@theoriginals.in>
2010-12-20 00:22:44 -0500

    revert bitly.go

--- bitly.go
+++ bitly.go
@@ -13,8 +13,7 @@ func shorten(long string) (short string) {
 
 	long = http.URLEscape(long)
 
-	url := fmt.Sprintf("http://api.bit.ly/v3/shorten?login=%s&apiKey=%s&domain=bit.ly&longUrl=%s&format=json", login, key, long)
-
+	url := fmt.Sprintf("http://api.bit.ly/v3/shorten?login=%s&apiKey=%s&longUrl=%s&format=json", login, key, long)
 	r, _, err := http.Get(url)
         defer r.Body.Close()
 

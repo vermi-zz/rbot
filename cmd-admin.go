721bd71c1937ad79599e173c665d79e03ccfcb5c
Justin <sloth@theoriginals.in>
2011-01-30 00:39:24 -0500

    Changed binary in restart() from "rbot" to os.Args[0].

--- cmd-admin.go
+++ cmd-admin.go
@@ -27,19 +27,14 @@ func restart(conn *irc.Conn, nick *irc.Nick, args, channel string) {
 	owner, _ := auth.String(conn.Network, "owner")
 	if owner == user(nick) {
 		here, _ := os.Getwd()
-		_, err := exec.LookPath("./rbot")
-		if err != nil {
-			say(conn, channel, "Couldn't find binary.")
-			return
-		}
 		argv := []string{""}
 		envv := []string{""}
-		_, err = exec.Run("rbot", argv, envv, here, exec.PassThrough, exec.PassThrough, exec.PassThrough)
+		say(conn, channel, "Restarting.")
+		_, err := exec.Run(os.Args[0], argv, envv, here, exec.PassThrough, exec.PassThrough, exec.PassThrough)
 		if err != nil {
 			say(conn, channel, "Unable to start new process.")
 			return
 		}
-		say(conn, channel, "Restarting.")
 		os.Exit(0)
 	}
 }

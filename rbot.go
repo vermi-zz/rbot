8cfa319eba7057f9e012525398337d8525f2c112
vermi <sloth@theoriginals.in>
2011-07-22 19:57:15 -0400

    What is this justin.tv nonsense?

--- rbot.go
+++ rbot.go
@@ -96,6 +96,9 @@ func autojoin(conn *irc.Conn) {
 				conn.Join(split[1])
 			}
 
+			if annstream, _ := conf.Bool(s, "stream"); annstream {
+				announceJustinTv(conn, split[1])
+			}
 		}
 	}
 	go BanManager(conn)
